1c8eb2d9b73983debe032b87cb2360261badd505
manuel <delverme@equilibretechnologies.com>
2023-10-05 14:45:21 +0200

    [CLUSTER] baseline

---
+++ deployment/buddy.py
@@ -0,0 +1,74 @@
+import inspect
+import os
+import sys
+
+import experiment_buddy
+
+
+def buddy_setup(hyper, entity="delvermm"):
+    if not isinstance(hyper, dict):
+        hyper = vars(hyper)
+    experiment_buddy.register_defaults(hyper)
+    import wandb
+    wandb_kwargs = dict(
+        monitor_gym=False, entity=entity, settings=wandb.Settings(start_method="thread"), save_code=True)
+    # esh = ""
+    # hostname = ""
+    # sweep_config = ""
+    # hostname = "cc-beluga"
+    # hostname = "cc-cedar"
+    # hostname = "mila"
+    hostname = "mila"
+    sweep_config = ""
+    # sweep_config = "sweep.yaml"
+    # proc_num = -1
+    proc_num = 1
+    if sweep_config:
+        proc_num = 4
+
+    # hostname = "aws://t4g.micro"
+    if sys.gettrace() is not None and os.environ.get("BUDDY_DEBUG_DEPLOYMENT") is None:
+        hostname = ""
+        sweep_config = ""
+    esh = "\n".join(l.strip() for l in """
+    #SBATCH --cpus-per-task=8
+    #SBATCH --mem=64G
+    #SBATCH --time=12:00:00
+    #SBATCH --gres=gpu:32gb:1
+        """.strip().split("\n")
+                    ) + "\n"
+    extra_modules = None
+    if hostname == "mila":
+        esh += "#SBATCH --partition=long\n"
+        extra_modules = [
+            "anaconda/3",
+            "cuda/11.1",
+            "pytorch/1.8.1"
+        ]
+    elif "cc" in hostname:
+        esh += "#SBATCH --partition=cpubase_bycore_b4\n"
+        esh += "#SBATCH --account=rrg-dprecup\n"
+        # esh += "#SBATCH --account=rrg-bengioy-ad\n"
+        extra_modules = [
+            "anaconda/3",
+            # "pytorch/1.7", # CC doesn't have pytorch, should be a package
+            "cuda/11.1",
+            "pytorch/1.8.1"
+        ]
+    else:
+        esh = ""
+    # has_conda_env_param = inspect.signature(experiment_buddy.deploy).parameters.get("conda_env") is not None
+    has_conda_env_param = False
+    if has_conda_env_param:
+        tb = experiment_buddy.deploy(
+            hostname, wandb_kwargs=wandb_kwargs, extra_slurm_headers=esh, sweep_definition=sweep_config,
+            proc_num=proc_num,
+            extra_modules=extra_modules, conda_env="traces_llm"
+        )
+    else:
+        tb = experiment_buddy.deploy(
+            hostname, wandb_kwargs=wandb_kwargs, extra_slurm_headers=esh, sweep_definition=sweep_config,
+            proc_num=proc_num,
+            extra_modules=extra_modules
+        )
+    return tb
