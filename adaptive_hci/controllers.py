31dd5d3c1c98f313c4e6d217e85362911c73c55f
KilianFt <kilian.freitag@diefreitags.de>
2023-10-19 15:59:57 +0200

    remove old controller

--- adaptive_hci/controllers.py
+++ adaptive_hci/controllers.py
@@ -1,17 +1,13 @@
 import abc
-import numpy as np
 import torch
 from torch import Tensor
 from torch.functional import F
 from stable_baselines3 import PPO
-from torch.utils.data import DataLoader
 from torchmetrics import ExactMatch, F1Score
 from vit_pytorch import ViT
 import lightning.pytorch as pl
 
-# from train_general_model import main
-from .training import train_model
-from .datasets import EMGWindowsAdaptationDataset
+
 class BaseController:
     @abc.abstractmethod
     def deterministic_forward(self, x) -> torch.Tensor:
@@ -51,60 +47,6 @@ class RLSLController(BaseController, PPO):
         return dist.distribution.mean
 
 
-class SLOnlyController(BaseController):
-    def __init__(self, model_path, device='cpu', lr=1e-3, batch_size=64, epochs=1, n_frozen_layers=0):
-        self.device = device
-        self.batch_size = batch_size
-        self.epochs = epochs
-
-        if model_path is not None:
-            self.policy = torch.load(model_path).to(self.device)
-        else:
-            raise NotImplementedError('policy cannot be trained in controller')
-            # self.policy = main()
-
-        if n_frozen_layers >= 1:
-            for i, param in enumerate(self.policy.to_patch_embedding.parameters()):
-                param.requires_grad = False
-            for i, param in enumerate(self.policy.dropout.parameters()):
-                param.requires_grad = False
-
-        if n_frozen_layers >= 2:
-            for layer_idx in range(min((n_frozen_layers - 1), 4)):
-                for i, param in enumerate(self.policy.transformer.layers[layer_idx].parameters()):
-                    param.requires_grad = False
-
-        self.model_name = model_path.split('/')[-1].split('.')[0]
-
-        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=lr)
-        self.criterion = torch.nn.MSELoss()
-
-    def deterministic_forward(self, emg_window):
-        emg_window_tensor = emg_window.unsqueeze(0).unsqueeze(0).to(self.device)
-        with torch.no_grad():
-            outputs = self.policy(emg_window_tensor)
-        return outputs
-
-    def sl_update(self, states, optimal_actions):
-        train_dataset = EMGWindowsAdaptationDataset(windows=states, labels=optimal_actions)
-
-        train_dataloader = DataLoader(train_dataset,
-                                      batch_size=self.batch_size,
-                                      shuffle=True,)
-
-        # train model
-        # TODO change to lightning
-        self.policy, history = train_model(self.policy,
-                                    optimizer=self.optimizer,
-                                    criterion=self.criterion,
-                                    train_dataloader=train_dataloader,
-                                    model_name=self.model_name,
-                                    device=self.device,
-                                    epochs=self.epochs,)
-
-        return history['train_loss'][-1]
-
-
 class EMGViT(ViT):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -113,7 +55,7 @@ class EMGViT(ViT):
         x.unsqueeze_(axis=1)
         return self.forward(x)
 
-# TODO combine with controller above
+
 class PLModel(pl.LightningModule):
     def __init__(self, model, n_labels, lr=1e-3, n_frozen_layers=None):
         super(PLModel, self).__init__()
