1a8fe3d6b839428a4f91cbf6623252c2bebc2de7
manuel <delverme@equilibretechnologies.com>
2023-10-24 15:35:49 +0200

    pydantic

--- configs.py
+++ configs.py
@@ -1,12 +1,17 @@
-import dataclasses
 import hashlib
 import pickle
+from typing import Literal
+
+from pydantic import BaseModel, Field
 
 from common import DataSourceEnum
 
+ConfigType = Literal['base', 'smoke']
 
-@dataclasses.dataclass
-class BaseConfig:
+
+class BaseConfig(BaseModel):
+    config_type: ConfigType = 'base'
+
     data_source: DataSourceEnum = DataSourceEnum.MAD
     pretrained: bool = False
     early_stopping: bool = False
@@ -46,28 +51,31 @@ class BaseConfig:
     sweep_config: str = ""
     proc_num: int = 1
     loss: str = "MSELoss"
-    train_fraction: float = 0.8  # 80% of the data for training
+    train_fraction: float = Field(0.8, description="80% of the data for training")
     limit_train_batches: int = 200
     finetune_num_workers: int = 8
     online_adaptation_num_workers: int = 8
 
-    def __post_init__(self):
-        # assert if all parameters have a typehint
-        all_vars = dir(self)
-        parameters = [var for var in all_vars if not callable(getattr(self, var)) and not var.startswith('_')]
-        assert len(parameters) == len(dataclasses.fields(self)), 'Some config parameters do not have a type, please specify'
+    class Config:
+        validate_assignment = True
 
+    def __init__(self, **data):
+        if 'data_source' in data and isinstance(data['data_source'], str):
+            enum_value = data['data_source'].split('.')[-1]
+            data['data_source'] = DataSourceEnum[enum_value]
+
+        super().__init__(**data)
         if self.sweep_config:
             self.proc_num = 4
 
     def __str__(self):
-        arg_str = pickle.dumps(self)
+        arg_str = pickle.dumps(self.model_dump())
         self_hash = hashlib.sha256(arg_str).hexdigest()[:15]
         return f"{self.__class__.__name__}({self_hash})"
 
 
-@dataclasses.dataclass
 class SmokeConfig(BaseConfig):
+    config_type: ConfigType = 'smoke'
     pretraining_epochs: int = 1
     batch_size: int = 1
     patch_size: int = 8
@@ -82,10 +90,10 @@ class SmokeConfig(BaseConfig):
 
     finetune_num_episodes: int = 2
     finetune_epochs: int = 2
-    finetune_num_workers = 0
+    finetune_num_workers: int = 0
     online_num_episodes: int = 1
     online_epochs: int = 1
     online_train_intervals: int = 1
     online_first_training_episode: int = 0
-    limit_train_batches = 1
-    online_adaptation_num_workers = 1
+    limit_train_batches: int = 1
+    online_adaptation_num_workers: int = 1
