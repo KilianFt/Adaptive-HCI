ead000dfb2f6ce56f11b1e9af39689e9f371c0c4
KilianFt <kilian.freitag@diefreitags.de>
2023-10-31 10:10:07 +0100

    remove configs

--- adaptive_hci/controllers.py
+++ adaptive_hci/controllers.py
@@ -20,9 +20,10 @@ class PLModel(pl.LightningModule):
         self.save_hyperparameters(ignore=['model'])
         self.model = model
         self.lr = lr
+        self.threshold = threshold
         self.criterion = torch.nn.MSELoss()
-        self.exact_match = ExactMatch(task="multilabel", num_labels=n_labels, threshold=0.5)
-        self.f1_score = F1Score(task="multilabel", num_labels=n_labels, threshold=0.5)
+        self.exact_match = ExactMatch(task="multilabel", num_labels=n_labels, threshold=threshold)
+        self.f1_score = F1Score(task="multilabel", num_labels=n_labels, threshold=threshold)
         self.accuracy_metric = Accuracy(task='binary')
 
         if n_frozen_layers is not None:
@@ -53,11 +54,11 @@ class PLModel(pl.LightningModule):
         self.log("train/loss", loss)
         return loss
 
-    def get_per_label_accuracies(self, outputs, targets, threshold = 0.5):
+    def get_per_label_accuracies(self, outputs, targets):
         num_targets = targets.shape[1]
 
-        binary_outputs = (outputs >= threshold).int()
-        binary_targets = (targets >= threshold).int()
+        binary_outputs = (outputs >= self.threshold).int()
+        binary_targets = (targets >= self.threshold).int()
 
         per_labels_accuracies = []
 
