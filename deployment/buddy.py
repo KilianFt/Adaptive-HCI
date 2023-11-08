0029b4c0eba6929ebcfd9d66455e70986674e2e3
manuel <delverme@equilibretechnologies.com>
2023-11-08 13:06:12 +0100

    [CLUSTER] no_id

--- deployment/buddy.py
+++ deployment/buddy.py
@@ -49,19 +49,10 @@ def buddy_setup(exp_config: configs.BaseConfig, entity):
         ]
     else:
         esh = ""
-    # has_conda_env_param = inspect.signature(experiment_buddy.deploy).parameters.get("conda_env") is not None
-    has_conda_env_param = False
-    if has_conda_env_param:
-        tb = experiment_buddy.deploy(
-            hostname, wandb_kwargs=wandb_kwargs, extra_slurm_headers=esh, sweep_definition=sweep_config,
-            proc_num=proc_num,
-            extra_modules=extra_modules, conda_env="traces_llm"
-        )
-    else:
-        tb = experiment_buddy.deploy(
-            hostname, wandb_kwargs=wandb_kwargs, extra_slurm_headers=esh, sweep_definition=sweep_config,
-            proc_num=proc_num,
-            extra_modules=extra_modules
-        )
+    tb = experiment_buddy.deploy(
+        hostname, wandb_kwargs=wandb_kwargs, extra_slurm_headers=esh, sweep_definition=sweep_config,
+        proc_num=proc_num,
+        extra_modules=extra_modules, conda_env="python39"
+    )
     updated_exp_config = exp_config.__class__(**tb.run.config)
     return tb, updated_exp_config
