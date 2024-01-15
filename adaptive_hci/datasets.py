c541a67932b61d1d0413af46d582894ad674a359
KilianFt <kilian.freitag@diefreitags.de>
2024-01-15 16:26:53 +0100

    add first version of autoregressive drawing model

--- adaptive_hci/datasets.py
+++ adaptive_hci/datasets.py
@@ -7,7 +7,10 @@ import subprocess
 import time
 import random
 from typing import Optional
+from pathlib import Path
+from collections import defaultdict
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from scipy.io import loadmat
@@ -529,3 +532,135 @@ def maybe_download_drive_folder(destination_folder, file_ids):
     assert len(filenames) == len(file_ids), 'Not all files exist {filenames}'
 
     return sorted(filenames)
+
+
+
+# TODO move to own file
+# Omniglot
+def load_img(fn):
+	I = plt.imread(fn)
+	I = np.array(I,dtype=bool)
+	return I
+
+
+def load_motor(fn):
+	motor = []
+	with open(fn,'r') as fid:
+		lines = fid.readlines()
+	lines = [l.strip() for l in lines]
+	for myline in lines:
+		if myline =='START': # beginning of character
+			stk = []
+		elif myline =='BREAK': # break between strokes
+			stk = np.array(stk)
+			stk[:,1] = -stk[:,1]
+			motor.append(stk) # add to list of strokes
+			stk = []
+		else:
+			arr = np.fromstring(myline,dtype=float,sep=',')
+			stk.append(arr)
+	return motor
+
+def num2str(idx):
+	if idx < 10:
+		return '0'+str(idx)
+	return str(idx)
+
+
+def get_raw_omniglot_dataset(stroke_dir, img_dir):
+    # TODO remove img?
+    n_characters = len([x for x in img_dir.glob('*')])
+
+    dataset = []
+    for char_int in range(1, n_characters + 1):
+        character_id = num2str(char_int)
+        stroke_char_dir = stroke_dir / ('character' + character_id)
+        img_char_dir = img_dir / ('character' + character_id)
+
+        stroke_files = [x for x in stroke_char_dir.glob('*')]
+        char_id = stroke_files[0].name[:4]
+        n_samples = len(stroke_files)
+
+        character_data = defaultdict(list)
+        for i in range(1, n_samples+1):
+            character_stroke_file = stroke_char_dir / (char_id + '_' + num2str(i) + '.txt')
+            character_img_file = img_char_dir / (char_id + '_' + num2str(i) + '.png')
+            motor = load_motor(character_stroke_file)
+            img = load_img(character_img_file)
+
+            character_data['img'].append(img)
+            character_data['motor'].append(motor)
+
+        dataset.append(character_data)
+    return dataset
+
+
+def sequence_to_moves(sequence, canvas_size=30, max_initial_value=120):
+    # TODO make parameter for step size
+    sequence = (sequence / max_initial_value) * canvas_size
+    sequence = sequence.astype(np.int32)
+
+    moves = []
+
+    for i in range(len(sequence) - 1):
+        point = sequence[i]
+        next_point = sequence[i+1]
+        # switch x and y
+        x_diff = next_point[1] - point[1]
+        y_diff = next_point[0] - point[0]
+        while abs(x_diff) > 0 or abs(y_diff) > 0:
+            if abs(y_diff) > abs(x_diff) or abs(y_diff) == abs(x_diff):
+                y_step = -1 if y_diff < 0 else 1
+                moves.append([0, y_step])
+                y_diff -= y_step
+            elif abs(y_diff) < abs(x_diff):
+                x_step = -1 if x_diff < 0 else 1
+                moves.append([x_step, 0])
+                x_diff -= x_step
+            else:
+                print('what', x_diff, y_diff)
+
+    moves = np.array(moves)
+    return moves
+
+
+def omniglot_dataset_to_moves(dataset, canvas_size=30, max_initial_value=120):
+    moves_data = []
+    for char_samples in dataset:
+        for char_sample in char_samples['motor']:
+            for stroke_sample in char_sample:
+                sequence = stroke_sample[:,:2]
+                moves = sequence_to_moves(sequence, canvas_size=canvas_size,
+                                          max_initial_value=max_initial_value)
+                moves_data.append(moves)
+    return moves_data
+
+
+def encode_moves(moves_data, move_map):
+    encoded_moves_data = []
+    for moves in moves_data:
+        # filter strokes with less than 5 points
+        if moves.shape[0] > 5:
+            encoded_moves = torch.zeros(moves.shape[0], dtype=torch.float32)
+            for i, point in enumerate(moves):
+                label = move_map.index(point.tolist())
+                encoded_moves[i] = label
+
+            encoded_moves_data.append(encoded_moves)
+    return encoded_moves_data    
+
+
+def get_omniglot_moves(omniglot_dir: Path, canvas_size: int = 30, max_initial_value: int = 120):
+    img_dir = omniglot_dir / 'python' / 'images_background' / 'Latin'
+    stroke_dir = omniglot_dir / 'python' / 'strokes_background' / 'Latin'
+    raw_dataset = get_raw_omniglot_dataset(stroke_dir, img_dir)
+    moves_data = omniglot_dataset_to_moves(raw_dataset, canvas_size=canvas_size,
+                                           max_initial_value=max_initial_value)
+
+    move_map = [[-1, 0],
+                [0, -1],
+                [1, 0],
+                [0, 1],
+                [0, 0]]
+    encoded_moves_data = encode_moves(moves_data, move_map)
+    return encoded_moves_data
--- auto_drawer.py
+++ auto_drawer.py
@@ -1,34 +1,161 @@
-class StrokeDataset(Dataset):
-    def __init__(self, images, pixels):
-        self.images = torch.tensor(images, dtype=torch.int32)
-        self.pixels = torch.tensor(pixels, dtype=torch.float32)
-    def __len__(self):
-        return len(self.images)
 
-    def __getitem__(self, index) -> tuple[torch.Tensor]:
-        img = self.images[index]
-        px = self.pixels[index]
-        return img, px
+import time
+import math
+import os
+from pathlib import Path
+from typing import Tuple
+
+import torch
+from torch import Tensor
+from torch import nn
+from tempfile import TemporaryDirectory
+from sklearn.model_selection import train_test_split
+
+from adaptive_hci.datasets import get_omniglot_moves
+from adaptive_hci.controllers import TransformerModel
 
 
-def get_omniglot_dataset(dataset_path):
+def batchify(data: Tensor, bsz: int, device: str) -> Tensor:
+    """Divides the data into ``bsz`` separate sequences, removing extra elements
+    that wouldn't cleanly fit.
 
-    canvas_data = []
-    pixel_data = []
-    for character_data in dataset:
-        for stroke_sample in character_data['motor']:
-            sequence = get_sequence(stroke_sample)
-            canvas_list, pixel_list = process_training_samples(sequence, canvas_size)
-            canvas_data += canvas_list
-            pixel_data += pixel_list
+    Arguments:
+        data: Tensor, shape ``[N]``
+        bsz: int, batch size
 
-    canvas_data = np.array(canvas_data)
-    pixel_data = np.array(pixel_data)
+    Returns:
+        Tensor of shape ``[N // bsz, bsz]``
+    """
+    seq_len = data.size(0) // bsz
+    data = data[:seq_len * bsz]
+    data = data.view(bsz, seq_len).t().contiguous()
+    return data.to(device)
 
-    dataset = StrokeDataset(images=canvas_data, pixels=pixel_data)
+
+def get_batch(source: Tensor, i: int, bptt: int) -> Tuple[Tensor, Tensor]:
+    """
+    Args:
+        source: Tensor, shape ``[full_seq_len, batch_size]``
+        i: int
+
+    Returns:
+        tuple (data, target), where data has shape ``[seq_len, batch_size]`` and
+        target has shape ``[seq_len * batch_size]``
+    """
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].reshape(-1)
+    return data, target
+
+
+def train(model: nn.Module, train_data, criterion, optimizer, scheduler, bptt, ntokens, epoch) -> None:
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 200
+    start_time = time.time()
+
+    num_batches = len(train_data) // bptt
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i, bptt)
+        output = model(data)
+        output_flat = output.view(-1, ntokens)
+        loss = criterion(output_flat, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            total_loss = 0
+            start_time = time.time()
+
+
+def evaluate(model: nn.Module, eval_data: Tensor, criterion, ntokens, bptt) -> float:
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    with torch.no_grad():
+        for i in range(0, eval_data.size(0) - 1, bptt):
+            data, targets = get_batch(eval_data, i, bptt)
+            seq_len = data.size(0)
+            output = model(data)
+            output_flat = output.view(-1, ntokens)
+            total_loss += seq_len * criterion(output_flat, targets).item()
+    return total_loss / (len(eval_data) - 1)
+
 
 def main():
-    dataset = get_omniglot_dataset(dataset_path)
+    canvas_size = 30
+    omniglot_dir = Path('./datasets/omniglot')
+    encoded_moves_data = get_omniglot_moves(omniglot_dir, canvas_size=canvas_size, max_initial_value=120)
+
+    move_map = [[-1, 0],
+                [0, -1],
+                [1, 0],
+                [0, 1],
+                [0, 0]]
+    
+    # TODO show result in end
+    # plot_encoded_moves(encoded_moves_data[6], move_map, canvas_size=30)
+
+    encoded_data_with_stop = [torch.cat((x, torch.tensor([4]))) for x in encoded_moves_data]
+
+    train_list, val_list = train_test_split(encoded_data_with_stop)
+
+    flat_train_data = torch.concat(train_list).type(torch.int)
+    flat_val_data = torch.concat(val_list).type(torch.int)
+
+    ntokens = 5
+    device = 'mps'
+    lr = 5.0  # learning rate
+    batch_size = 20
+    eval_batch_size = 10
+    bptt = 35
+
+    train_data = batchify(flat_train_data, batch_size, device=device)  # shape ``[seq_len, batch_size]``
+    val_data = batchify(flat_val_data, eval_batch_size, device=device)
+
+    model = TransformerModel(ntoken=ntokens, d_model=4, nhead=2, d_hid=128,
+                            nlayers=1, device=device).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+    best_val_loss = float('inf')
+    epochs = 30
+
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
+
+        for epoch in range(1, epochs + 1):
+            epoch_start_time = time.time()
+            train(model, train_data, criterion, optimizer, scheduler,
+                  bptt, ntokens, epoch)
+            val_loss = evaluate(model, val_data, criterion, ntokens, bptt)
+            val_ppl = math.exp(val_loss)
+            elapsed = time.time() - epoch_start_time
+            print('-' * 89)
+            print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+                f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
+            print('-' * 89)
+
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                torch.save(model.state_dict(), best_model_params_path)
+
+            scheduler.step()
+        model.load_state_dict(torch.load(best_model_params_path)) # load best model states
+
+    torch.save(model, 'models/drawer_test.pt')
+
 
 if __name__ == '__main__':
     main()
